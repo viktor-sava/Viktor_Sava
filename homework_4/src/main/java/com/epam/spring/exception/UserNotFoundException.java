3c5aac1953de22b7ba52312b2132fbbe979742b5
Viktor Sava <sleep632@gmail.com>
2022-02-08 12:49:05 +0200

    Override method of exceptions

--- homework_4/src/main/java/com/epam/spring/exception/UserNotFoundException.java
+++ homework_4/src/main/java/com/epam/spring/exception/UserNotFoundException.java
@@ -1,7 +1,14 @@
 package com.epam.spring.exception;
 
+import com.epam.spring.service.enums.ErrorType;
+
 public class UserNotFoundException extends ServiceException{
     public UserNotFoundException() {
         super("User with the such email is not found");
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.VALIDATION_ERROR_TYPE;
     }
 }
