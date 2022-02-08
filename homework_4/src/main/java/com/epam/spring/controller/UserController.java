52177a2e17e113162a8bc93ee8401a12255ae406
Viktor Sava <sleep632@gmail.com>
2022-02-08 13:16:32 +0200

    Add groups for validation, add validation marks to UserDto

--- homework_4/src/main/java/com/epam/spring/controller/UserController.java
+++ homework_4/src/main/java/com/epam/spring/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.epam.spring.controller;
 
 import com.epam.spring.controller.dto.UserDto;
+import com.epam.spring.controller.dto.group.OnCreate;
+import com.epam.spring.controller.dto.group.OnUpdate;
 import com.epam.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,15 +30,22 @@ public class UserController {
         return userService.getUser(email);
     }
 
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{email}/block")
+    public UserDto blockUser(@PathVariable String email) {
+        userService.blockUser(email);
+        return userService.getUser(email);
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public UserDto createUser(@RequestBody UserDto userDto) {
+    public UserDto createUser(@RequestBody @Validated(OnCreate.class) UserDto userDto) {
         return userService.createUser(userDto);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{email}")
-    public UserDto updateUser(@PathVariable String email, @RequestBody UserDto userDto) {
+    public UserDto updateUser(@PathVariable String email, @RequestBody @Validated (OnUpdate.class) UserDto userDto) {
         return userService.updateUser(email, userDto);
     }
 
--- homework_4/src/main/java/com/epam/spring/controller/dto/ReceiptItemDto.java
+++ homework_4/src/main/java/com/epam/spring/controller/dto/ReceiptItemDto.java
@@ -1,14 +1,19 @@
 package com.epam.spring.controller.dto;
 
+import com.epam.spring.controller.dto.group.OnCreate;
 import lombok.Builder;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 public class ReceiptItemDto {
 
+    @NotNull(message = "'product' shouldn't be empty", groups = OnCreate.class)
     private ProductDto product;
 
+    @NotNull(message = "'product' shouldn't be empty", groups = OnCreate.class)
     private Integer quantity;
 
 }
